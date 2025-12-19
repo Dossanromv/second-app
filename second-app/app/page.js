8ee1c8fac0483a6a3b7a631de0424cc4334ae4ce
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2025-12-19 10:01:42 +0500

    Настройка стилей и центровка текста

--- second-app/app/page.js
+++ second-app/app/page.js
@@ -2,16 +2,38 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+
+
+<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+
+<div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  
+  <div>
+    <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+    <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
+  </div>
+</div>
+
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-90 px-16 bg-white dark:bg-black sm:items-start">
+      
+
+
+{/* -------------------------- The Image -------------------------------------------------------------------------------*/}
+
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
+          src="/starkiko.svg"
+          alt="Company logo"
+          width={140}
           height={20}
           priority
         />
+
+
+
+
+{/* -------------------------- The Button -------------------------------------------------------------------------------*/}
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.js file.
@@ -35,12 +57,17 @@ export default function Home() {
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+
+
+
+
+{/* -------------------------- The Button -------------------------------------------------------------------------------*/}
+
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <Image
               className="dark:invert"
               src="/vercel.svg"
@@ -48,8 +75,10 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy Now
+            Sing now
           </a>
+
+
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -58,6 +87,13 @@ export default function Home() {
           >
             Documentation
           </a>
+
+
+
+{/* -------------------------- The End -------------------------------------------------------------------------------*/}
+       
+
+
         </div>
       </main>
     </div>
