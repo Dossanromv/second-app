6ec5f89458dd6cb134e7c8f0674cc282ff276596
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2025-12-24 11:19:54 +0500

    обноваление

--- second-app/app/page.js
+++ second-app/app/page.js
@@ -1,102 +1,27 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
-
-
-<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
-<div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  
-  <div>
-    <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
-    <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
-  </div>
-</div>
-
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-90 px-16 bg-white dark:bg-black sm:items-start">
+<div className="min-h-screen flex items-center justify-center bg-white dark:bg-black transition-all duration-300 px-4">
       
+      <div className="max-w-3xl w-full text-center space-y-10">
+        
+        <h1 className="text-4xl md:text-6xl font-semibold text-black dark:text-white leading-tight">
+          Next.js Dark Mode Tutorial
+        </h1>
+        
+        <p className="text-lg text-zinc-600 dark:text-zinc-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio facilis 
+          assumenda atque ad tempore nihil maxime doloribus aut ab quibusdam commodi, 
+          quidem nemo odit veniam eum temporibus molestias, quaerat dicta.
+        </p>
+        
+        
+        <button className="px-8 py-3 bg-black dark:bg-white text-white dark:text-black rounded-full hover:scale-105 transition-transform">
+          Начать обучение
+        </button>
 
-
-{/* -------------------------- The Image -------------------------------------------------------------------------------*/}
-
-        <Image
-          className="dark:invert"
-          src="/starkiko.svg"
-          alt="Company logo"
-          width={140}
-          height={20}
-          priority
-        />
-
-
-
-
-{/* -------------------------- The Button -------------------------------------------------------------------------------*/}
-
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-
-
-
-
-{/* -------------------------- The Button -------------------------------------------------------------------------------*/}
-
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer">
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Sing now
-          </a>
-
-
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-
-
-
-
-{/* -------------------------- The End -------------------------------------------------------------------------------*/}
-       
-
-
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
